6f77e9c379633dcbce54cd9e94d4d7fa630d8931
fenris <Fenris-v>
2020-12-15 21:22:08 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+    /**
+     * SLICK
+     */
+    $('.firstScreen__slider').slick({
+        infinite: true,
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+        speed: 1000
+    });
+});
