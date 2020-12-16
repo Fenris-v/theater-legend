2df60cde814ac8e5a74602fc7a81b5f1d69b2b0b
fenris <Fenris-v>
2020-12-16 20:40:18 +0300

    adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,12 @@ $(document).ready(() => {
         fade: true,
         speed: 1000
     });
+
+    /**
+     * HAMBURGER
+     */
+    $('.menu-toggle-inner').on('click', function (e) {
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('.headerNav_adaptive').toggleClass('header_opened');
+    });
 });
