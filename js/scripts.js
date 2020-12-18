0349765adcefc197e07d10eb50720071ea3f6280
fenris <Fenris-v>
2020-12-18 21:58:17 +0300

    add any blocks

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,31 @@ $(document).ready(() => {
         speed: 1000
     });
 
+    $('.infoBlock_half__slider').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1000
+    });
+
     /**
      * HAMBURGER
      */
-    $('.menu-toggle-inner').on('click', function (e) {
+    $('.menu-toggle-inner').on('click', function () {
         $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
         $('.headerNav_adaptive').toggleClass('header_opened');
+    });
+
+    /**
+     * ANCHOR
+     */
+    $('.anchorLink').on('click', (e) => {
+        e.preventDefault();
+        let id = $(e.target).attr('href');
+        let top = $(id).offset().top - 20;
+        $('body, html').animate({scrollTop: top}, 1500);
     });
 });
